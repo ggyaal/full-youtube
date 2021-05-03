3027b40f23f6851c54de09a7baa34b6030c0d76a
ggyaal <72740806+ggyaal@users.noreply.github.com>
2021-05-04 02:27:05 +0900

    local & templates variables at pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) => res.render("join", { pageTitle: "join" });
+export const login = (req, res) => res.render("login", { pageTitle: "login" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "logout" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "user" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "edit profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "change pwd" });
