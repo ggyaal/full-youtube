a38873f354d068c3b851e2fa3f179142be9bb991
ggyaal <72740806+ggyaal@users.noreply.github.com>
2021-05-04 01:43:12 +0900

    divide controller & router for MVC

--- app.js
+++ app.js
@@ -3,21 +3,20 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from app");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
-app.get("/", handleHome);
 
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
