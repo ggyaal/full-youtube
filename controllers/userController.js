8bad6fb3e6085d1c21f120ce425fc47f001699f7
ggyaal <72740806+ggyaal@users.noreply.github.com>
2021-05-05 02:17:43 +0900

    Upload other things

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,9 +18,17 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "login" });
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "logout" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "user" });
 export const editProfile = (req, res) =>
