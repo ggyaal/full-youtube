5fd94ba4f4d7fbe483d48152fa202a04f856bab4
ggyaal <ggyaal@gmail.com>
2021-05-07 16:51:26 +0900

    add video edit, delete(not repo)

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "../routes";
 import {
   videoDetail,
-  editVideo,
+  getEditVideo,
+  postEditVideo,
   deleteVideo,
   getUpload,
   postUpload,
@@ -11,10 +12,18 @@ import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
