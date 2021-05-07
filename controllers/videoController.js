95a4212af8fbf79e6e74dd382f18ced24c35e0dd
ggyaal <ggyaal@gmail.com>
2021-05-07 21:55:22 +0900

    add search fun

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -11,11 +11,19 @@ export const home = async (req, res) => {
   }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const getUpload = (req, res) =>
