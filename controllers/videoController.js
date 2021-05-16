e7c1c44ca93bc915dd614293a5b3fc6dba89f505
ggyaal <ggyaal@gmail.com>
2021-05-16 16:53:08 +0900

    add video creator

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -38,7 +38,10 @@ export const postUpload = async (req, res) => {
     fileUrl: path,
     title,
     description,
+    creator: req.user.id,
   });
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -47,7 +50,7 @@ export const videoDetail = async (req, res) => {
     params: { id },
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
