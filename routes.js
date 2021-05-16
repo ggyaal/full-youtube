c303be95e079de3635d896e7168055a7abecbd76
ggyaal <ggyaal@gmail.com>
2021-05-16 22:52:12 +0900

    add video views ( views + 1 when video watch and

--- routes.js
+++ routes.js
@@ -31,6 +31,11 @@ const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
+// API
+
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+
 const routes = {
   home: HOME,
   join: JOIN,
@@ -71,6 +76,8 @@ const routes = {
   me: ME,
   facebook: FACEBOOK,
   facebookCallback: FACEBOOK_CALLBACK,
+  api: API,
+  registerView: REGISTER_VIEW,
 };
 
 export default routes;
