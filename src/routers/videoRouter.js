0abdce79638588a5bf31b054d37405219fac0469
ggyaal <ggyaal@gmail.com>
2021-06-30 00:10:52 +0900

    add profile & another

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
   deleteVideo,
-  updateVideo,
   getUploadVideo,
   view,
   postUploadVideo,
@@ -10,11 +9,10 @@ import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.VIEW, view);
-videoRouter.get(routes.UPDATE_VIDEO, updateVideo);
-videoRouter.get(routes.DELETE_VIDEO, deleteVideo);
+videoRouter.get(routes.VIEW(), view);
+videoRouter.get(routes.DELETE_VIDEO(), deleteVideo);
 videoRouter
-  .route(routes.UPDATE_VIDEO)
+  .route(routes.UPDATE_VIDEO())
   .get(getUploadVideo)
   .post(postUploadVideo);
 
